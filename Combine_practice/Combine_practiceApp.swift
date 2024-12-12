13bfdf722aee201690861fee377f33877b03c2f0
avan31017 <avan31017@gmail.com>
2024-12-12 16:41:47 +0900

    first commit Combine_practice

---
+++ Combine_practice/Combine_practiceApp.swift
@@ -0,0 +1,32 @@
+//
+//  Combine_practiceApp.swift
+//  Combine_practice
+//
+//  Created by 조성빈 on 12/12/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Combine_practiceApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
